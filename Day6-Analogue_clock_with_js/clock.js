dd88b479da3acc89492d46192fb4db687a738c0b
thisisjoyjacob <thisisjoyjacob@gmail.com>
2020-07-03 22:58:35 +0530

    at last the project got completed

--- Day6-Analogue_clock_with_js/clock.js
+++ Day6-Analogue_clock_with_js/clock.js
@@ -6,11 +6,11 @@ ctx.translate(radius,radius);
 radius = radius*0.90;
 setInterval(drawClock, 1000);
 
-drawClock();
+//drawClock();
 
 function drawClock() {
     drawFace(ctx, radius);
-    drawNumber(ctx, radius);
+    drawNumbers(ctx, radius);
     drawTime(ctx, radius);
 }
 
@@ -33,7 +33,7 @@ function drawFace(ctx,radius) {
     ctx.fill();
 }
 
-function drawNumber(ctx, radius) {
+function drawNumbers(ctx, radius) {
     var ang;
     var num;
     ctx.font = radius*0.15 + "px arial";
@@ -51,14 +51,30 @@ function drawNumber(ctx, radius) {
     }
 }
 
+function drawTime(ctx, radius){
+  var now = new Date();
+  var hour = now.getHours();
+  var minute = now.getMinutes();
+  var second = now.getSeconds();
+  //hour
+  hour = hour%12;
+  hour = (hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
+  drawHand(ctx, hour, radius*0.5, radius*0.07);
+  //minute
+  minute = (minute*Math.PI/30)+(second*Math.PI/(30*60));
+  drawHand(ctx, minute, radius*0.8, radius*0.07);
+  // second
+  second = (second*Math.PI/30);
+  drawHand(ctx, second, radius*0.9, radius*0.02);
+}
 
-function drawHand(ctx, pos, Length, width) {
-    ctx.beginPath();
-    ctx.lineWidth = width;
-    ctx.lineCap = "round";
-    ctx.moveTo(0,0);
-    ctx.rotate(pos);
-    ctx.lineTo(0, -length);
-    ctx.stroke();
-    ctx.rotate(-pos);
+function drawHand(ctx, pos, length, width) {
+  ctx.beginPath();
+  ctx.lineWidth = width;
+  ctx.lineCap = "round";
+  ctx.moveTo(0,0);
+  ctx.rotate(pos);
+  ctx.lineTo(0, -length);
+  ctx.stroke();
+  ctx.rotate(-pos);
 }
