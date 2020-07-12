14bc8e4aea614f3847daf6249380997f032755c3
thisisjoyjacob <thisisjoyjacob@gmail.com>
2020-07-13 00:14:52 +0530

    js changes

--- Day21-Alarm_with_js/myscript.js
+++ Day21-Alarm_with_js/myscript.js
@@ -20,13 +20,13 @@ var currentTime = setInterval(function(){
     var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
 
     //convert military time to standard time
-    if (hours <0 ){
-        hours = hours * -1;
-    } else if (hours == 00) {
-        hours = 12;
-    } else {
-        hours = hours;
-    }
+   // if (hours <0 ){
+   //     hours = hours * -1;
+    // } else if (hours == 00) {
+        // hours = 12;
+    // } else {
+        // hours = hours;
+    // }
 
     h2.textContent = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + " " + ampm;
 
@@ -38,57 +38,104 @@ function addZero(time) {
 } 
 
 
-/* 
 
-
-
-/* 
-
-var thr = createSel(23);
-document.getElementById("tpick-h").appendChild(thr);
-var thm = createSel(59);
-document.getElementById("tpick-m").appendChild(thm);
-var ths = createSel(59);
-document.getElementById("tpick-s").appendChild(ths);
-
-var tset = document.getElementById("tset");
-tset.addEventListener("click", set);
-var treset = document.getElementById("treset");
-treset.addEventListener("click", reset);
-
-*/
-
-// var sound = document.getElementById("alarm-sound");
-
-// var alarm = null;
-// setInterval(tick,1000);
-
-
-var createSel = function(max) {
-    // support function - creates a selector for hr, min, sec
-
-    var selector = document.createElement("select");
-    for (var i=0; i<=max; i++) {
+var  init = function () {
+    // init() : start the alarm clock
+  
+      // The time picker - Hr, Min, Sec
+      thr = createSel(23);
+      document.getElementById("tpick-h").appendChild(thr);
+      thm = createSel(59);
+      document.getElementById("tpick-m").appendChild(thm);
+      ths = createSel(59);
+      document.getElementById("tpick-s").appendChild(ths);
+  
+      // The time picker - Set, reset
+      tset = document.getElementById("tset");
+      tset.addEventListener("click", set);
+      treset = document.getElementById("treset");
+      treset.addEventListener("click", reset);
+  
+      // The alarm sound
+      sound = document.getElementById("alarm-sound");
+  
+      // Start the clock
+      alarm = null;
+      setInterval(tick, 1000);
+    }
+  
+  var  createSel = function (max) {
+    // createSel() : support function - creates a selector for hr, min, sec
+  
+      var selector = document.createElement("select");
+      for (var i=0; i<=max; i++) {
         var opt = document.createElement("option");
         i = padzero(i);
         opt.value = i;
         opt.innerHTML = i;
         selector.appendChild(opt);
+      }
+      return selector
     }
-    return selector
-}
+  
+   var padzero = function (num) {
+    // padzero() : support function - pads hr, min, sec with 0 if <10
+  
+      if (num < 10) { num = "0" + num; }
+      else { num = num.toString(); }
+      return num;
+    }
+  
+  var  tick = function () {
+    // tick() : update the current time
+  
+      // Current time
+      var now = new Date();
+      var hr = padzero(now.getHours());
+      var min = padzero(now.getMinutes());
+      var sec = padzero(now.getSeconds());
+  
+      // Update current clock
+      chr.innerHTML = hr;
+      cmin.innerHTML = min;
+      csec.innerHTML = sec;
+  
+      // Check and sound alarm
+      if (alarm != null) {
+        now = hr + min + sec;
+        if (now == alarm) {
+          if (sound.paused) {
+            sound.play();
+          }
+        }
+      }
+    }
+  
+   var set = function () {
+    // set() : set the alarm
+  
+      alarm = thr.value + thm.value + ths.value;
+      thr.disabled = true;
+      thm.disabled = true;
+      ths.disabled = true;
+      tset.disabled = true;
+      treset.disabled = false;
+    }
+  
+  var  reset = function () {
+    // reset() : reset the alarm
+  
+      if (!sound.paused) {
+        sound.pause();
+      }
+      alarm = null;
+      thr.disabled = false;
+      thm.disabled = false;
+      ths.disabled = false;
+      tset.disabled = false;
+      treset.disabled = true;
+    }
 
-var padzero =  function(num) {
-    // support funtion - pads hr, min, sec with 0 if <10
 
-    if (num < 10) {num = "0" + num; }
-    else ( num = num.toString(); )
-    return num;
-}
-
-var tick = function() {
-    
-}
-
-
-*/
+// INIT - RUN ALARM CLOCK
+window.addEventListener("load", init);
