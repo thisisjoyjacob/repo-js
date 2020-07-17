2870d3eca83a8ab9a77f654ce10d1fea69a79c7a
thisisjoyjacob <thisisjoyjacob@gmail.com>
2020-07-17 23:24:21 +0530

    making HTML5 logo

---
+++ Day26-HTML5_logo_project/myscript.js
@@ -0,0 +1,87 @@
+var ctx;
+var factorvalue=1;
+var fontfamily = "65px 'Gill Sans Ultra Bold', sans-serif";
+
+function inti() {
+    ctx=document.getElementById('canvas').getContext('2d');
+    ctx.font = fontfamily;
+    dologo();
+}
+
+/** dologo function defition. This is the main funtion. It uses factorvalue to change the scale */
+function dologo() {
+    var offsety = 80;
+    ctx.restore();
+    ctx.save();
+    ctx.clearRect(0,0,600,400);
+    ctx.scale(factorvalue,factorvalue);
+    ctx.fillText("HTML", 31,60);
+    ctx.translate(0,offsety);
+    
+    // 5 sided orange background
+    ctx.fillStyle = "#E34C26";
+    ctx.beginPath();
+    ctx.moveTo(39,250);
+    ctx.lineTo(17,0);
+    ctx.lineTo(262,0);
+    ctx.lineTo(239,250);
+    ctx.lineTo(139,278);
+    ctx.closePath();
+    // right hand, lighter orange part of the background
+    ctx.fillstyle = "#F06529"; // official darker color
+    ctx.beginPath();
+    ctx.moveTo(139, 257);
+    ctx.lineTo(220, 234);
+    ctx.lineTo(239, 20);
+    ctx.lineTo(139, 20);
+    ctx.closePath();
+    ctx.fill();
+    //light gray, left hand side part of the five
+    ctx.fillstyle = "#EBEBEB"; // gray color
+    ctx.beginPath();
+    ctx.moveTo(139, 113);
+    ctx.lineTo(98, 113);
+    ctx.lineTo(96, 82);
+    ctx.lineTo(139, 82);
+    ctx.lineTo(139, 51);
+    ctx.lineTo(62, 51);
+    ctx.lineTo(70, 144);
+    ctx.lineTo(139, 144);
+    ctx.closePath();
+    ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(139, 193);
+    ctx.lineTo(105, 184);
+    ctx.lineTo(103, 159);
+    ctx.lineTo(72, 159);
+    ctx.lineTo(76, 207);
+    ctx.lineTo(139, 225);
+    ctx.fill();
+    // white, right hand side of the 5
+    ctx.fillstyle = "#FFFFFF";
+    ctx.beginPath();
+    ctx.moveTo(139, 113);
+    ctx.lineTo(139, 144);
+    ctx.lineTo(177, 144);
+    ctx.lineTo(173, 184);
+    ctx.lineTo(139, 193);
+    ctx.lineTo(139, 225);
+    ctx.lineTo(202, 207);
+    ctx.lineTo(210, 113);
+    ctx.closePath();
+    ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(139, 51);
+    ctx.lineTo(139, 82);
+    ctx.lineTo(213, 82);
+    ctx.lineTo(216, 51);
+    ctx.closePath();
+    ctx.fill();
+}
+
+// the changescale function, response to user input.
+ function changescale(val){
+     factorvalue = val / 100;
+     
+     dologo();
+ }
